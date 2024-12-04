402c5f27c3811702f477936515906f4ce34be975
James Cauwelier <james@accelerated.dev>
2024-12-04 15:35:36 -0800

    Update prelude

--- specler/src/prelude.rs
+++ specler/src/prelude.rs
@@ -1,4 +1,5 @@
 pub use crate::core::create_with_specification::CreateWithSpecification;
+pub use crate::core::require::Require;
 pub use crate::core::spec::Spec;
-pub use crate::core::spec::contains_spec;
+pub use crate::core::spec::contains_spec::ContainsSpec;
 pub use crate::core::spec_error::SpecError;
